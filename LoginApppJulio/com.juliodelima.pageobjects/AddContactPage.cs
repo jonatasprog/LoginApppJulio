35e99bdef61438be94bc78eac3fd9b77df4d3b7a
marciodb <43177141+marciodb@users.noreply.github.com>
2018-09-17 11:54:52 -0300

    Driver Chrome
    Adicionado o driver do Chrome.

--- LoginApppJulio/com.juliodelima.pageobjects/AddContactPage.cs
+++ LoginApppJulio/com.juliodelima.pageobjects/AddContactPage.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace LoginApppJulio.com.juliodelima.pageobjects
 {
